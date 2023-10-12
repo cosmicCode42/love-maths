710570fb47b653555eb04dfb8c39bb4460f3eadc
cosmicCode42 <cosmicanswer@gmail.com>
2023-10-12 18:51:39 +0000

    added code for runGame and display addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,14 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}!`)
+                runGame(gameType);
             }
         })
     }
+
+    // Set the initial game type to 'addition'.
+    runGame("addition");
+
 })
 
 // Going to use Math.random() to generate numbers between 1 and 25. Let's make an attempt at code that can work...
@@ -43,10 +47,38 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game 'loop', called when the script is first loaded and
  * after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
+    
     // Uses my version of the random number generation between 1 and 25.
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    // checks game type - my attempt! Currently commented so as not to wreck stuff.
+    // switch (gameType) {
+    //     case 'addition':
+    //         displayAdditionQuestion(num1, num2);
+    //         break;
+    //     case 'subtract':
+    //         displaySubtractQuestion(num1, num2);
+    //         break;
+    //     case 'multiply':
+    //         displayMultiplyQuestion(num1, num2);
+    //         break;
+    //     case 'division':
+    //         displayDivisionQuestion(num1, num2);
+    //         break;
+    //     default:
+    //         alert(`Unknown game type: ${gameType}.`)
+    //         throw `Unknown game type: ${gameType}. Aborting!`
+    // }
+
+    // original code at this point in the lesson:
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}.`)
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -65,8 +97,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    // Question setup - gets the two numbers and the operator
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
