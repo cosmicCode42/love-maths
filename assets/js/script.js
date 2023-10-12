1db713a5ad1b081d083d80b7d151a874c82cda8d
cosmicCode42 <cosmicanswer@gmail.com>
2023-10-12 18:03:02 +0000

    coded the skeleton for the JS script

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+/*
+  We need:
+  - runGame(): runs the game
+  - checkAnswer(): checks the answer given
+  - calculateCorrectAnswer(): calculates and returns the correct answer and the game type to checkAnswer()
+  - incrementScore(): increments Correct Answers if answer is correct
+  - incrementWrongAnswer(): increments Incorrect Answers if answer is wrong
+  - displayAdditionQuestion(): displays the addition question and changes game type
+  - displaySubtractQuestion(): displays the subtraction question and changes game type
+  - displayMultiplyQuestion(): displays the multiplication question and changes game type
+  - displayDivisionQuestion(): displays the division question and changes game type
+*/
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+// This is the one I did.
+function displayDivisionQuestion() {
+
+}
